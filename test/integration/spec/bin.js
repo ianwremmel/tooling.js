ba4552000c4ece2691e84818e7e8dfd141556cf5
Ian W. Remmel <design@ianwremmel.com>
2016-11-26 22:44:30 -0500

    chore(tests): adjust test timeout to address perf on circleci

--- test/integration/spec/bin.js
+++ test/integration/spec/bin.js
@@ -6,6 +6,10 @@ const path = require(`path`);
 
 describe(`bin`, () => {
   it(`accepts a script file as its first parameter`, () => {
+    // between the babel-register compile time and the delay in the fixture, two
+    // seconds doesn't quite cut it on circle ci
+    this.timeout(5000);
+
     const stdout = cp.execSync(`bin/tooling ./test/integration/fixtures/example.js`, {
       cwd: path.resolve(__dirname, `../../..`)
     }).toString();
