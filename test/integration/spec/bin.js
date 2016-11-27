76c8060af6888294afd535683e7a3c37f77f83d1
Ian W. Remmel <design@ianwremmel.com>
2016-11-26 22:46:39 -0500

    chore(test): use functions instead of arrow functions for timeout-adjusted tests

--- test/integration/spec/bin.js
+++ test/integration/spec/bin.js
@@ -5,7 +5,7 @@ const cp = require(`child_process`);
 const path = require(`path`);
 
 describe(`bin`, () => {
-  it(`accepts a script file as its first parameter`, () => {
+  it(`accepts a script file as its first parameter`, function() {
     // between the babel-register compile time and the delay in the fixture, two
     // seconds doesn't quite cut it on circle ci
     this.timeout(5000);
